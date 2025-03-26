2d0f5692c5533c9df7669fc609ce7e0ca4f606a5
Lilan Abeywickrama <lileelakshi@gmail.com>
2025-03-26 23:03:53 +0530

    Show hidden files in explorer

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -5,3 +5,18 @@
     opts = { style = "day" },
   },
 }--]]
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        hidden = true,
+        ignored = true,
+      },
+      picker = {
+        hidden = true,
+        ignored = true,
+      },
+    },
+  },
+}
